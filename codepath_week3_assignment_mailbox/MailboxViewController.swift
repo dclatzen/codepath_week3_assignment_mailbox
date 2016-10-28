c2adc7033754b353967a2a56d0a609006661f7ec
dclatzen <devon@studyblue.com>
2016-10-27 18:08:47 -0700

    Figured out scrolling and menu edge swipe. Not following the assignment instructions perfectly yet though.

--- codepath_week3_assignment_mailbox/MailboxViewController.swift
+++ codepath_week3_assignment_mailbox/MailboxViewController.swift
@@ -2,99 +2,126 @@
 //  MailboxViewController.swift
 //  codepath_week3_assignment_mailbox
 //
-//  Created by StudyBlue on 10/26/16.
-//  Copyright © 2016 myself. All rights reserved.
-//
+
 
 import UIKit
 
-class MailboxViewController: UIViewController, UIScrollViewDelegate {
+class MailboxViewController: UIViewController, UIGestureRecognizerDelegate {
     
-    @IBOutlet weak var feedScrollView: UIScrollView!
-    @IBOutlet weak var feedImageView: UIImageView!
-    @IBOutlet weak var rescheduleImageView: UIImageView!
-    @IBOutlet weak var listImageView: UIImageView!
-    @IBOutlet weak var messageImageView: UIImageView!
+    // Message to pan
     @IBOutlet weak var messageParentView: UIView!
+    @IBOutlet weak var messageImageView: UIImageView!
+    
+    
+    // Icons
     @IBOutlet weak var laterImageView: UIImageView!
     @IBOutlet weak var archiveImageView: UIImageView!
-    @IBOutlet weak var menuHiderView: UIView!
     
+    
+    // Feed items
+    @IBOutlet weak var feedScrollView: UIScrollView!
+    @IBOutlet weak var feedImageView: UIImageView!
+
+    
+    // "reschedule" and "list" screens
+    @IBOutlet weak var rescheduleImageView: UIImageView!
+    @IBOutlet weak var listImageView: UIImageView!
+    
+    @IBOutlet weak var menuImageView: UIImageView!
+    
+
     var messageOriginalX: CGFloat!
     var laterImageViewOriginalX: CGFloat!
     var archiveImageViewOriginalX: CGFloat!
-    var menuHiderViewOriginalX: CGFloat!
+    var feedScrollViewOriginalX: CGFloat!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        // Establish initial values
         
-        // Establish initial position values
         messageOriginalX = messageImageView.frame.origin.x
+        
         laterImageViewOriginalX = laterImageView.frame.origin.x
         archiveImageViewOriginalX = archiveImageView.frame.origin.x
-        menuHiderViewOriginalX = menuHiderView.frame.origin.x
+        
+        feedScrollViewOriginalX = feedScrollView.frame.origin.x
         
         rescheduleImageView.alpha = 0
         listImageView.alpha = 0
         
-        feedScrollView.contentSize = CGSize(width: 375, height: 1634)
-        menuHiderView.frame.size = CGSize(width: 375, height: 1634)
-        feedScrollView.delegate = self
+        menuImageView.alpha = 0
         
-        let panGestureRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(didPanFeedScroller(sender:)))
-        panGestureRecognizer.edges = UIRectEdge.left
-        
-        menuHiderView.isUserInteractionEnabled = true
-        menuHiderView.addGestureRecognizer(panGestureRecognizer)
+        feedScrollView.contentSize = CGSize(width: 375, height: 2435)
         
         
+        // Set up Screen Edge Pan Gesture Recognizer
+        
+        let edgePanGestureRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(didPanFeedScrollView(sender:)))
+        
+        edgePanGestureRecognizer.edges = UIRectEdge.left
+        
+        
+        feedScrollView.isUserInteractionEnabled = true
+        feedScrollView.addGestureRecognizer(edgePanGestureRecognizer)
+        
+        edgePanGestureRecognizer.delegate = self
         
     }
     
     
     @IBAction func didPanMessage(_ sender: UIPanGestureRecognizer) {
+        
+        ////////////////////////////////
+        ////// Message Pan Setup ///////
+        ////////////////////////////////
+        
         let translation = sender.translation(in: view)
         
-        // swiping messageImageView left: map negative translation.x to a positive value between 0 and 1 to assign to the alpha
+        // Left pan -> later_icon.alpha: map negative translation.x to a value between 0.2 and 1, for assigning to the "later" icon alpha
         var tXLeftForAlpha = convertValue(inputValue: translation.x, r1Min: -20, r1Max: -100, r2Min: 0.2, r2Max: 1)
         
-        // swiping messageImageView right: map translation.x to a value between 0 and 1 to assign to the alpha
+        // Right pan -> archive_icon.alpha: map positive translation.x to a value between 0.2 and 1, for assigning to the "archive" icon alpha
         var txRightForAlpha = convertValue(inputValue: translation.x, r1Min: 20, r1Max: 100, r2Min: 0.2, r2Max: 1)
-        
-        
+
         if sender.state == .began {
             
-            // set initial alpha for the reschedule icon
+            // set initial states
             laterImageView.alpha = 0.2
             archiveImageView.alpha = 0.2
             
             laterImageView.image = UIImage(named: "later_icon")
             archiveImageView.image = UIImage(named: "archive_icon")
+            
             messageParentView.backgroundColor = UIColor.lightGray
             
             
         } else if sender.state == .changed {
             
-            // track the message with the pan position
+            // move the message with the pan position
             messageImageView.frame.origin.x = CGFloat(messageOriginalX + translation.x)
             
-            // fade in the later icon
+            // prepare to fade in the "later" icon
             laterImageView.alpha = tXLeftForAlpha
             
-            //fade in the archive icon
+            //prepare to fade in the "archive" icon
             archiveImageView.alpha = txRightForAlpha
             
-            // grey, default
+            ///////////////////////////////
+            ////// Message Pan Left ///////
+            ///////////////////////////////
+            
+            // grey, default. -60 < x < 60
             if messageImageView.frame.origin.x < 60 && messageImageView.frame.origin.x > -60 {
                 
                 UIView.animate(withDuration: 0.5, animations: {
                     self.messageParentView.backgroundColor = UIColor.lightGray
                 })
+            }
                 
-                // yellow, "later" action
-            } else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x > -260 {
+            // yellow, "later" action. -260 < x < 0
+            else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x > -260 {
                 
                 archiveImageView.alpha = 0
                 
@@ -105,8 +132,10 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
                 laterImageView.image = UIImage(named: "later_icon")
                 laterImageView.frame.origin.x = laterImageViewOriginalX + (translation.x + 60)
                 
-                // brown, list icon
-            } else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x > -375 {
+            }
+            
+            // brown, list icon. -375 < x < -260
+            else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x > -375 {
                 
                 archiveImageView.alpha = 0
                 
@@ -117,8 +146,15 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
                 laterImageView.image = UIImage(named: "list_icon")
                 laterImageView.frame.origin.x = laterImageViewOriginalX + (translation.x + 60)
                 
-                // green, archive
-            } else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 260 {
+                
+            }
+                
+            ////////////////////////////////
+            ////// Message Pan Right ///////
+            ////////////////////////////////
+                
+            // green, archive. 0 < x < 260
+            else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 260 {
                 
                 UIView.animate(withDuration: 0.5, animations: {
                     self.messageParentView.backgroundColor = UIColor.green
@@ -126,10 +162,10 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
                 
                 archiveImageView.image = UIImage(named: "archive_icon")
                 archiveImageView.frame.origin.x = archiveImageViewOriginalX + (translation.x - 60)
-                
-                
-                // red, x delete
-            } else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 375 {
+            }
+            
+            // red, x delete. 260 < x < 375
+            else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 375 {
                 
                 UIView.animate(withDuration: 0.3, animations: {
                     self.messageParentView.backgroundColor = UIColor.red
@@ -143,36 +179,45 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
             
         } else if sender.state == .ended {
             
+            ///////////////////////////////////////////
+            ////// Message Pan Ended: No Action ///////
+            ///////////////////////////////////////////
+            
             // when the user chooses nothing, reset everything
             if messageImageView.frame.origin.x < 60 && messageImageView.frame.origin.x > -60 {
+                
                 UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: [], animations: {
+                    
                     self.messageImageView.frame.origin.x = self.messageOriginalX
                     self.laterImageView.frame.origin.x = self.laterImageViewOriginalX
                     self.archiveImageView.frame.origin.x = self.archiveImageViewOriginalX
-                    }, completion: { (Bool) in
-                })
+                    })
                 
-                // user chooses yellow, later icon
+            //////////////////////////////////////
+            ////// Message Pan Ended: Left ///////
+            //////////////////////////////////////
+                
+            // LATER: user chooses yellow, later icon. -260 < x < 0
             }  else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x > -260 {
                 
                 // animate the message to the side
                 UIView.animate(withDuration: 0.2, animations: {
+                    
                     self.messageImageView.frame.origin.x = -(self.messageImageView.frame.width * 0.7)
                     self.laterImageView.frame.origin.x = ((self.laterImageViewOriginalX + 60) - (self.messageImageView.frame.width * 0.7))
-                    print ("message x: \(self.messageImageView.frame.origin.x)")
                 })
-                
                 
                 // delay, then animate the Reschedule image in
                 UIView.animate(withDuration: 0.3, delay: 0.2, animations: {
                     self.rescheduleImageView.alpha = 1
                 })
                 
-                // user chooses brown, list icon
+            // LIST: user chooses brown, list icon. -375 < x < -260
             } else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x > -375 {
                 
                 // animate the message to the side
                 UIView.animate(withDuration: 0.2, animations: {
+                    
                     self.messageImageView.frame.origin.x = -(self.messageImageView.frame.width * 0.8)
                     self.laterImageView.frame.origin.x = ((self.laterImageViewOriginalX + 60) - (self.messageImageView.frame.width * 0.8))
                 })
@@ -182,13 +227,18 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
                     self.listImageView.alpha = 1
                 })
                 
+            ///////////////////////////////////////
+            ////// Message Pan Ended: Right ///////
+            ///////////////////////////////////////
                 
-                // user chooses green, archive icon
+            // ARCHIVE: user chooses green, archive icon. 0 < x < 260
             }  else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 260 {
                 
                 // animate the message out
                 UIView.animate(withDuration: 0.2, animations: {
+                    
                     self.messageImageView.frame.origin.x = self.messageOriginalX + self.messageImageView.frame.width
+                    
                     self.archiveImageView.frame.origin.x = ((self.archiveImageViewOriginalX - 60) + self.messageImageView.frame.width)
                 })
                 
@@ -198,12 +248,14 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
                 })
                 
                 
-                // user chooses red, x delete icon
+            // DELETE: user chooses red, x delete icon. 260 < x < 375
             }  else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 375 {
                 
                 // animate the message out
                 UIView.animate(withDuration: 0.2, animations: {
+                    
                     self.messageImageView.frame.origin.x = self.messageOriginalX + self.messageImageView.frame.width
+                    
                     self.archiveImageView.frame.origin.x = ((self.archiveImageViewOriginalX - 60) + self.messageImageView.frame.width)
                 })
                 
@@ -218,86 +270,97 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
         
     }
     
-    // dismiss Reschedule view
+    // dismiss the Reschedule view
     @IBAction func didTapReschedule(_ sender: UITapGestureRecognizer) {
         
         // Fade out the Reschedule view
         UIView.animate(withDuration: 0.3, animations: {
             self.rescheduleImageView.alpha = 0
+            
         }) { (Bool) in
             // and then complete the message slide-out animation
             UIView.animate(withDuration: 0.2, animations: {
+                
                 self.messageImageView.frame.origin.x -= self.messageImageView.frame.width * 0.3
                 self.laterImageView.frame.origin.x -= self.messageImageView.frame.width * 0.3
             })
-            
         }
         
         // move up the feed to close the gap
         UIView.animate(withDuration: 0.5, delay: 0.6, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: [], animations: {
             self.feedImageView.frame.origin.y -= self.messageImageView.frame.height
-        }) { (Bool) in
-            
-        }
-        // end didTapReschedule
-    }
+        })
+    
+    } // end didTapReschedule
     
     
     @IBAction func didTapListImage(_ sender: UITapGestureRecognizer) {
         
-        // fade out the List view, and then complete the message slide-out animation
+        // fade out the List view
         UIView.animate(withDuration: 0.3, animations: {
+            
             self.listImageView.alpha = 0
+            
         }) { (Bool) in
             // and then complete the message slide-out animation
             UIView.animate(withDuration: 0.2, animations: {
+                
                 self.messageImageView.frame.origin.x -= self.messageImageView.frame.width * 0.2
                 self.laterImageView.frame.origin.x -= self.messageImageView.frame.width * 0.2
             })
-            
         }
         
         // move up the feed to close the gap
         UIView.animate(withDuration: 0.5, delay: 0.6, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: [], animations: {
             self.feedImageView.frame.origin.y -= self.messageImageView.frame.height
-        }) { (Bool) in
-            
-        }
-        // end didTapReschedule
+        })
+        
+    } // end didTapReschedule
+    
+    
+    // allow multiple gestures to affect feedScrollView
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
     
-    
-    func didPanFeedScroller (sender: UIPanGestureRecognizer) {
+    // swipe from the edge to reveal the menu
+    func didPanFeedScrollView (sender: UIScreenEdgePanGestureRecognizer) {
+        
         let location = sender.location(in: view)
         
         if sender.state == .began {
             
+            menuImageView.alpha = 1
+            
         } else if sender.state == .changed {
-            menuHiderView.frame.origin.x = menuHiderViewOriginalX + location.x
+            
+            feedScrollView.frame.origin.x = feedScrollViewOriginalX + location.x
+            
         } else if sender.state == .ended {
             
+            // snap back if not panned far enough
             if location.x < 150 {
+                
                 UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: [], animations: {
-                    self.menuHiderView.frame.origin.x = self.menuHiderViewOriginalX
-                })
+                    
+                    self.feedScrollView.frame.origin.x = self.feedScrollViewOriginalX
+                }) { (Bool) in
+                    self.menuImageView.alpha = 0
+                }
+            }
+                
+            // snap to the right if they did pan far enough
+            else {
 
-            } else {
                 UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: [], animations: {
-                    self.menuHiderView.frame.origin.x = self.menuHiderViewOriginalX + (self.menuHiderView.frame.width * 0.93)
-                    })
+                    
+                    self.feedScrollView.frame.origin.x = self.feedScrollViewOriginalX + (self.feedScrollView.frame.width * 0.93)
+                })
             }
             
         }
         
-        
-        
-        print ("location.x: \(location.x)")
-    }
-    
-    
-    
-    
-    
+    } // end didPanMenuHider
     
     
 }
